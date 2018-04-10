fdf8c2d3eefc108deb0d729c8f5a0a2f3ecb76ff
unknown <hilt@uw.edu>
2018-04-09 17:12:49 -0700

    completed exercises

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,7 +3,7 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100")
 # Hint: use the `paste()` function and vector recycling to add a number to the word
 # "Employee"
-employees <- paste("Employee ", 1:100)
+employees <- paste("Employee", 1:100)
 
 # Create a vector of 100 random salaries for the year 2017
 # Use the `runif()` function to pick random numbers between 40000 and 50000
@@ -16,15 +16,16 @@ salaries_2018 <- salaries_2017 - runif(100, min = -5000, max = 10000)
 
 # Create a data frame 'salaries' by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
-salaries <- data.frame(employees, salaries_2017, salaries_2018, stringsAsFactors = F)
+salaries <- data.frame(employees, salaries_2017, salaries_2018,
+                       stringsAsFactors = F)
 
 # Create a column 'change' that stores each person's change in salary between
 # 2017 and 2018
-
+salaries$change <- salaries_2018 - salaries_2017
 
 # Create a column 'got_raise' that is TRUE if the person got a raise (their
 # salary went up)
-
+salaries$got_raise <- salaries_2018 > salaries_2017
 
 
 ### Retrieve values from your data frame to answer the following questions
@@ -32,7 +33,7 @@ salaries <- data.frame(employees, salaries_2017, salaries_2018, stringsAsFactors
 ### cell rather than the whole row!)
 
 # What was the 2018 salary of employee 57
-
+salaries[salaries$employees == "Employee 57",s]
 
 # How many employees got a raise?
 
